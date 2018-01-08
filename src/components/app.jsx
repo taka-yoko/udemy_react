0cbc1d5e64a876f971e1543822a8f703cec5f587
横山貴芳 <t-yoko@t-yoko.local>
2018-01-08 16:50:17 +0900

    get geometory

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-import Greeting from './greeting';
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Bob',
     };
   }
 
-  handleNameChange(name) {
-    this.setState({name: name});
+  setMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
+  handlePlaceSubmit(place) {
+    axios.get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        const result = results.data.results[0];
+        const status = results.data.status;
+        switch(status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setMessage('エラーが発生しました');
+            break;
+          }
+        } 
+      })
+      .catch((error) => {
+        this.setMessage('通信エラーが発生しました');
+      })
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)} />
-        <Greeting name={this.state.name} />
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </div>
     );
   }
