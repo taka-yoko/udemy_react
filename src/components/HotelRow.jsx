9dae6cafea63e9e0cfee280550fb38802277ad6b
横山貴芳 <t-yoko@t-yoko.local>
2018-01-21 17:12:24 +0900

    表示する情報の追加

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 
 const HotelRow = ({ hotel }) => (
   <tr>
+    <td><img src={hotel.thumbnail} alt={hotel.name} /></td>
     <td><a href={hotel.url} target="_blank">{hotel.name}</a></td>
+    <td>{hotel.price}</td>
+    <td>{hotel.reviewAverage}</td>
+    <td>{hotel.reviewCount}</td>
+    <td>{hotel.distance}</td>
   </tr>
 );
 
@@ -12,6 +17,11 @@ HotelRow.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
     url: PropTypes.string,
+    price: PropTypes.string,
+    thumbnail: PropTypes.string,
+    reviewAverage: PropTypes.number,
+    reviewCount: PropTypes.number,
+    distance: PropTypes.number,
   }).isRequired,
 };
 
--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,4 @@
+import geolib from 'geolib';
 import Rakuten from '../lib/Rakuten';
 
 const RAKUTEN_APP_ID = '1053951556666558390';
@@ -12,13 +13,22 @@ export const searchHotelByLocation = (location) => {
   };
   return Rakuten.Travel.simpleHotelSearch(params)
     .then((result) => {
-      console.log(result);
       return result.data.hotels.map((hotel) => {
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        )
         return {
           id: basicInfo.hotelNo,
           name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
+          price: price ? `${price}円` : '空室なし',
+          thumbnail: basicInfo.hotelThumbnailUrl,
+          reviewAverage: basicInfo.reviewAverage,
+          reviewCount: basicInfo.reviewCount,
+          distance,
         }
       });
     });
